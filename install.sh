a50bd401eba6fd2ad56ee1797ef86db901b2189e
alph <perrydf85@gmail.com>
2025-12-29 16:11:15 -0800

    First Commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#Install script for Alph's Dotfiles
+
+
+	#Create dir environment
+
+
+	#Waybar Config
+
+fc-list | grep -i nerd
+sudo pacman -S ttf-jetbrains-mono-nerd
+
+git clone https://github.com/soaddevgit/WaybarTheme.git
+cp WaybarTheme/* ~/.config/waybar/
+
+	#Keybind Config
+
+
+	#Hyprpaper Config
+
+sudo pacman -S hyprpaper
+
